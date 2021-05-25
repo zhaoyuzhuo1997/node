983b7db191a388e6c7a54bdaa999b32bbbe59c4e
zhaoyuzhuo1997 <zhaoyuzhuo1997@gmaile.com>
2021-05-25 18:12:08 +0900

    관리자 이력서 양식 작업

--- routes/admin.js
+++ routes/admin.js
@@ -5,8 +5,10 @@ const express = require('express');
 const router = express.Router();
 
 router.get("/", (req, res, next) => {
-	
-	return res.send("관리자 페이지");
+	const params = {
+		addClass : 'admin_page',
+	};
+	return res.render("admin/main", params);
 });
 
 module.exports = router;
