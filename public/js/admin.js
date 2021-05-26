7fa63841e6a84287d9ccf841cf1c6176c0615aaf
zhaoyuzhuo1997 <zhaoyuzhuo1997@gmaile.com>
2021-05-26 16:14:19 +0900

    학력, 경력 양식 작업

--- public/js/admin.js
+++ public/js/admin.js
@@ -1,23 +1,22 @@
-
 /**
 * 프로필 사진 업로드 처리
 *
 * @param Boolean isSuccess - true 업로드 성공, false - 실패
 */
-function uploadCallback(isSuccess)
+function uploadCallback(isSuccess) 
 {
 	/**
-		성공
-			1. 프로필 사진 -> 사진 추가 영역에 붙여넣기
-			2. 레이어 팝업 닫기
-			
-		실패
-			1. 업로드 실패 메세지 출력 후 레이어 팝업
+		성공 
+				1. 프로필 사진 -> 사진 추가 영역에 붙여 넣고 
+				2. 레이어 팝업 닫기
+				
+		실패 
+			1. 업로드 실패 메세지 출력 후 레이어 팝업 닫기
 	*/
-	if (isSuccess) { // 성공
+	if (isSuccess) { // 성공 
 		const tag = `<img src='/profile/profile'>`;
 		$(".photo_upload").html(tag);
-	} else { // 실패
+	} else { // 실패 
 		alert("이미지 업로드 실패");
 	}
 	
@@ -37,5 +36,37 @@ $(function() {
 				$("input[name='address']").val(data.address);
 			}
 		}).open();
+	});
+	
+	/** 양식 추가 처리 */
+	$(".add_form").click(function() {
+		const type = $(this).data("type");
+		let template = "";
+		switch (type) {
+			case "학력" : 
+				template = "school";
+				break;
+		}
+		
+		if (template) {
+			$html = $("#template_" + template).html();
+			$target = $(this).closest(".form_inner").find(".form_html");
+			$target.append($html);
+		} 
+	});
+	
+	/** 양식 제거 처리 */
+	$("body").on("click", ".form_html .remove", function() {
+		$(this).closest(".rows").remove();
+	});
+	
+	
+	/** textarea 확대 축소 처리 */
+	$("body").on("focus", ".form_html textarea", function() {
+		$(this).removeClass("h200").addClass("h200");
+	});
+	
+	$("body").on("blur", ".form_html textarea", function() {
+		$(this).removeClass("h200");
 	});
 });
